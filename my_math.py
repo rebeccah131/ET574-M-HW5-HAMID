09b2cf712b3e9df157d12af35bef3170a95a403d
Rebecca <rebeccahamid00@gmail.com>
2025-11-29 03:14:25 +0000

    fix indentation for lines 10 -15

--- my_math.py
+++ my_math.py
@@ -7,12 +7,12 @@ def is_Prime(x):
     except: 
             ValueError("x has to be an integer.")
 
-if x <= 1:              
-    return False   #x is is less than or equal to 1, which is not a prime number,neither are negative numbers.
-if x % 2 == 0:
-    return False   #even numbers are not prime numbers
-if x  == 2:
-     return True    #2 is the only even number that is a prime number.
+    if x <= 1:              
+        return False   #x is is less than or equal to 1, which is not a prime number,neither are negative numbers.
+    if x % 2 == 0:
+        return False   #even numbers are not prime numbers
+    if x  == 2:
+        return True    #2 is the only even number that is a prime number.
 
 
     
