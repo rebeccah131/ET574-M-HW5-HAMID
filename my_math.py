0c69a95cdc2be98cdc74d8359e266f20ae067158
Rebecca <rebeccahamid00@gmail.com>
2025-11-29 03:05:55 +0000

    added line 14&15

--- my_math.py
+++ my_math.py
@@ -11,7 +11,8 @@ if x <= 1:
     return False   #x is is less than or equal to 1, which is not a prime number,neither are negative numbers.
 if x % 2 == 0:
     return False   #even numbers are not prime numbers
-
+if x  == 2:
+     return True    #2 is the only even number that is a prime number.
 
 
     
