5eab64f3a6e8d79d00d800d7440e61fb5457722b
Rebecca <rebeccahamid00@gmail.com>
2025-11-29 04:08:49 +0000

    edited line 20, added lines 12&13, and lines 21-23

--- my_math.py
+++ my_math.py
@@ -7,18 +7,25 @@ def is_Prime(x):
     except: 
             ValueError("x has to be an integer.")
 
-    if x <= 1:              
-        return False   #x is is less than or equal to 1, which is not a prime number,neither are negative numbers.
+    if x <= 0:              
+        return False #x is is less than or equal to 0, which is not a prime number,neither are negative numbers.
+    if x == 1:
+         return False  #x cannot be 1 since it is not a prime number.
     if x % 2 == 0:
         return False   #even numbers are not prime numbers
     if x  == 2:
         return True    #2 is the only even number that is a prime number.
 
 #2 Original Math Functions
-def is_PerfectSquare(n):
-    if n < 0:
+def Perfect_Square(n):
+    try: 
+         n = int(n)
+    except: ValueError("n had to be an integer.")
+    if n <= 0:
         return False
     root = int(n ** 0.5)
     return root * root == n
+
+
 
 
